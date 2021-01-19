9741fd6aa3687bca296e1a6aae92db0c1562dcbe
Anuj Nayyar <anuj_n@hotmail.co.uk>
2021-01-19 16:39:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ function divide (a, b){
 }
 
 function increment(n){
-  return (n++)
+  n++
+  return n
 }
 
 function decrement(n){
-  return (n--)
+  n--
+  return n
 }
 
 function makeInt(int){
